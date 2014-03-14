83f67c55360ec9159485f684dbdd71dcaf09fb21
Anthony Deaver <lordbishop@gmail.com>
2014-03-13 23:23:15 -0400

    little tweaks

--- js/editor/editor.js
+++ js/editor/editor.js
@@ -1,63 +1,76 @@
-/// <reference path="jquery.d.ts" />
-
-var r;
-
-function setStat(user, stat, value) {
-    user[stat] = value;
-}
-
-var u = { stats: { hp: 0 } };
-setStat(u, "hp", 10);
-
-// User.stats.hp = 0;
-var Engine;
-(function (Engine) {
-    function parseConfig() {
+var Editor = (function () {
+    function Editor() {
+        var _this = this;
+        this._onFileChange = function (e) {
+            _this.onFileChange(e);
+        };
+        this._submitRoomConfig = function (e) {
+            _this.onSubmitRoomConfig(e);
+        };
     }
-    function log(str) {
-    }
-    Engine.log = log;
-    function start() {
-    }
-    Engine.start = start;
-})(Engine || (Engine = {}));
+    Editor.prototype.parseRooms = function (data) {
+        data = decodeURIComponent(data);
+        $('#selector').hide();
 
-var e = Engine;
+        var rooms = JSON.parse(data).rooms;
 
-e.start();
-
-var M;
-(function (M) {
-    M.a = 1;
-})(M || (M = {}));
-
-// var iEngine {
-var Engine2 = (function () {
-    function Engine2(start) {
-        this.start = start;
-        this.DEBUG = true;
-        this._modalPanel = $('<div>').attr('id', 'modal').addClass('eventPanel').css('opacity', '0');
-    }
-    Engine2.prototype.createButton = function (config) {
-        // var func: ();
-        return this._modalPanel;
+        for (var i = rooms.length - 1; i >= 0; i--) {
+            $('#roomList div').append($('<li />').attr('id', i).text(rooms[i].name));
+        }
+        $("#results").text(data);
     };
-    return Engine2;
+
+    Editor.prototype.queryToObj = function (str) {
+        var arr = str.split('&'), len = arr.length, obj = {}, val = [], i;
+
+        for (i = 0; i < len; i++) {
+            val = arr[i].split('=');
+            obj[val[0]] = val[1];
+        }
+
+        return obj;
+    };
+
+    Editor.prototype.registerEvents = function () {
+        $('files').on('change', this._onFileChange);
+        $('#roomMaker').on('submit', this._submitRoomConfig);
+    };
+
+    // Event Handlers
+    Editor.prototype.onFileChange = function (evt) {
+        console.log('selected');
+        var files = evt.target.files;
+        var contents;
+        for (var i = 0, f; f = files[i]; i++) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                contents = e.target.result;
+            };
+            reader.onloadend = function () {
+                this.parseRooms(contents);
+            };
+            reader.readAsText(f);
+        }
+    };
+
+    Editor.prototype.onSubmitRoomConfig = function (e) {
+        e.preventDefault();
+
+        // e.stopPropagation();
+        // console.log('e: ', e);
+        var str = $(this).serialize();
+        var t = this.queryToObj(str);
+        t.id = t.name.replace(/\W/g, '_');
+        t.name = t.name.replace(/\+/g, ' ');
+        var rooms = { "rooms": [t] };
+
+        var o = JSON.stringify(rooms);
+        var b = new Blob([o], { type: 'text/json' });
+        saveAs(b, "rooms.json");
+    };
+
+    Editor.prototype.init = function () {
+        this.registerEvents();
+    };
+    return Editor;
 })();
-
-// class User extends Engine {}
-var Student = (function () {
-    function Student(firstname, middleinitial, lastname) {
-        this.firstname = firstname;
-        this.middleinitial = middleinitial;
-        this.lastname = lastname;
-        this.fullname = firstname + " " + middleinitial + " " + lastname;
-    }
-    return Student;
-})();
-
-function greeter(person) {
-    return "Hello, " + person.firstname + " " + person.lastname;
-}
-
-var user = new Student("Jane", "M.", "User");
--- js/editor/editor.ts
+++ js/editor/editor.ts
@@ -2,11 +2,10 @@ declare var $;
 
 class Editor {
 
-	constructor() {
-		
-	}
-	
-	parseRooms(data: string) {
+	private _onFileChange = (e) => { this.onFileChange(e); }
+	private _submitRoomConfig = (e) => { this.onSubmitRoomConfig(e); }
+
+	private parseRooms(data: any) {
 		data = decodeURIComponent(data);
 		$('#selector').hide();
 
@@ -16,5 +15,61 @@ class Editor {
 		    $('#roomList div').append($('<li />').attr('id',i).text(rooms[i].name))
 		}
 		$( "#results" ).text( data );
+	}
+
+	private queryToObj(str) {
+		var arr = str.split('&'),
+			len = arr.length,
+			obj = {}, 
+			val = [],
+			i;
+
+		for(i= 0; i < len; i++) {
+			val = arr[i].split('=');
+			obj[val[0]] = val[1];
+		}
+
+		return obj;
+	}
+
+	private registerEvents() {
+		$('files').on('change', this._onFileChange);
+		$('#roomMaker').on('submit', this._submitRoomConfig);
+	}
+
+	// Event Handlers
+	private onFileChange(evt: any) {
+		console.log('selected');
+		var files = evt.target.files;
+		var contents;
+		for (var i = 0, f; f = files[i]; i++) {
+			var reader = new FileReader();
+			reader.onload = function(e) {
+				contents = e.target.result;
+			}
+			reader.onloadend = function() {
+				this.parseRooms(contents);	
+			}
+			reader.readAsText(f);
+		}
+	}
+
+	private onSubmitRoomConfig(e: any) {
+    	e.preventDefault();
+    	// e.stopPropagation();
+    	// console.log('e: ', e);
+    	var str = $( this ).serialize();
+    	var t = this.queryToObj(str);
+    	t.id = t.name.replace(/\W/g,'_');
+    	t.name = t.name.replace(/\+/g,' ')
+    	var rooms =  { "rooms" : [t] };
+
+    	var o = JSON.stringify(rooms);
+    	var b = new Blob([o],{type : 'text/json'});
+    	saveAs(b, "rooms.json");
+	}
+
+	public init() {
+		this.registerEvents();
 	}
 }
