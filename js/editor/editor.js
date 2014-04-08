063f2fef86ab0bc8a81fea9485cd4204985a2782
unknown <adeaver@adeaver-864.ad.wsicorp.com>
2014-04-08 11:34:24 -0400

    Ability to generate a list of rooms and save added

--- js/editor/editor.js
+++ js/editor/editor.js
@@ -4,25 +4,31 @@ var Editor = (function () {
         this._onFileChange = function (e) {
             _this.onFileChange(e);
         };
-        this._submitRoomConfig = function (e) {
-            _this.onSubmitRoomConfig(e);
+        this._createRoom = function (e) {
+            _this.onCreateRoom(e);
+        };
+        this._saveRooms = function (e) {
+            _this.saveRoomsFile();
         };
         this._form = document.getElementById('roomMaker');
         this._rooms = [];
         this.registerEvents();
     }
-    Editor.prototype.parseRooms = function (data) {
+    Editor.prototype.handleExistingRooms = function (data) {
         data = decodeURIComponent(data);
         $('#selector').hide();
-        $('#roomList').show();
-        $('#editor').show();
 
+        // $('#roomList').show();
+        // $('#editor').show();
         this._rooms = JSON.parse(data).rooms;
-        console.log('rooms: ', this._rooms);
+        this.listRooms();
+    };
+
+    Editor.prototype.listRooms = function () {
+        $('#roomList div').html('');
         for (var i = this._rooms.length - 1; i >= 0; i--) {
             $('#roomList div').append($('<li />').attr('id', i).text(this._rooms[i].name));
         }
-        $("#results").text(data);
     };
 
     Editor.prototype.queryToObj = function (str) {
@@ -49,38 +55,70 @@ var Editor = (function () {
                 contents = e.target.result;
             };
             reader.onloadend = function () {
-                that.parseRooms(contents);
+                that.handleExistingRooms(contents);
             };
             reader.readAsText(f);
         }
     };
 
-    Editor.prototype.onSubmitRoomConfig = function (e) {
-        console.log('submitting room');
+    Editor.prototype.onCreateRoom = function (e) {
         e.preventDefault();
-
-        // e.stopPropagation();
+        var required = ['name', 'desc'];
         var str = $('#roomMaker').serialize();
         var t = this.queryToObj(str);
-        console.log('this: ', this);
-        t.name = t.name.replace(/\W/g, '_');
-        t['name'] = t['name'].replace(/\+/g, ' ');
+        var r = {};
+
+        for (var x = 0; x < required.length; x++) {
+            if (t[required[x]].length < 1) {
+                this.alert('Missing Required field');
+                // return;
+            }
+        }
+
+        r.name = t.name;
+        r.short_code = t.name.replace(/\W/g, '_').toLowerCase();
+        r.desc = t.desc;
+
+        //r.short_code = r.name.toLowerCase()://replace(/\+/g,' ');
+        r.exits = [];
+
+        var exits = $("input[name='exits[]']:checked");
+        for (var e = 0; e < exits.length; e++) {
+            r.exits.push(exits[e].value);
+        }
+
+        if (r.exits.length === 0) {
+            this.alert('Rooms required exits');
+        }
 
         // Set Defaults
-        t['hasMonster'] = t['hasMonster'] || false;
-        t['hasTreasure'] = t['hasTreasure'] || false;
+        r.hasMonster = t.hasMonster || false;
+        r.hasTreasure = t.hasTreasure || false;
 
-        this._rooms.push(t);
+        this._rooms.push(r);
 
+        console.log('rooms: ', this._rooms);
+        this.listRooms();
+        // var o = JSON.stringify({ "rooms" : this._rooms });
+        // var b = new Blob([o],{type : 'text/json'});
+        // saveAs(b, "rooms.json");
+    };
+
+    Editor.prototype.saveRoomsFile = function () {
         var o = JSON.stringify({ "rooms": this._rooms });
         var b = new Blob([o], { type: 'text/json' });
         saveAs(b, "rooms.json");
+    };
+    Editor.prototype.alert = function (msg) {
+        console.log(msg);
+        $("#results").append($('<span />').html(msg));
     };
 
     Editor.prototype.registerEvents = function () {
         console.log('registering events');
         $('#files').on('change', this._onFileChange);
-        $('#roomMaker #save').on('click', this._submitRoomConfig);
+        $('#roomMaker #create').on('click', this._createRoom);
+        $('#save').on('click', this._saveRooms);
     };
     return Editor;
 })();
--- js/editor/editor.ts
+++ js/editor/editor.ts
@@ -6,20 +6,24 @@ class Editor {
 	private _form: HTMLElement;
 
 	private _onFileChange = (e) => { this.onFileChange(e); }
-	private _submitRoomConfig = (e) => { this.onSubmitRoomConfig(e); }
+	private _createRoom = (e) => { this.onCreateRoom(e); }
+	private _saveRooms = (e) => { this.saveRoomsFile(); }
 
-	private parseRooms(data: any) {
+	private handleExistingRooms(data: any) {
 		data = decodeURIComponent(data);
 		$('#selector').hide();
-		$('#roomList').show();
-		$('#editor').show();
+		// $('#roomList').show();
+		// $('#editor').show();
 
 		this._rooms = JSON.parse(data).rooms;
-		console.log('rooms: ', this._rooms);
+		this.listRooms();
+	}
+
+	private listRooms() {
+		$('#roomList div').html('');
 		for (var i = this._rooms.length - 1; i >= 0; i--) {
 		    $('#roomList div').append($('<li />').attr('id',i).text(this._rooms[i].name))
 		}
-		$( "#results" ).text( data );
 	}
 
 	private queryToObj(str) {
@@ -50,37 +54,71 @@ class Editor {
 				contents = e.target.result;
 			}
 			reader.onloadend = function() {
-				that.parseRooms(contents);	
+				that.handleExistingRooms(contents);	
 			}
 			reader.readAsText(f);
 		}
 	}
 
-	private onSubmitRoomConfig(e: any) {
-		console.log('submitting room');
+	private onCreateRoom(e: any) {
     	e.preventDefault();
-    	// e.stopPropagation();
+    	var required = ['name', 'desc'];
     	var str = $('#roomMaker').serialize();
-    	var t = this.queryToObj(str);
-    	console.log('this: ', this);
-    	t.name = t.name.replace(/\W/g,'_');
-    	t['name'] = t['name'].replace(/\+/g,' ')
+    	var t:any = this.queryToObj(str);
+    	var r:any = {};
+    	// Check for empty required fields.
+    	for (var x = 0; x < required.length; x++) {
+    		if (t[required[x]].length < 1) {  
+    			this.alert('Missing Required field');
+	    		// return;
+    		}
+    	}
+
+    	r.name = t.name;
+    	r.short_code = t.name.replace(/\W/g,'_').toLowerCase();
+    	r.desc = t.desc;
+    	//r.short_code = r.name.toLowerCase()://replace(/\+/g,' ');
+    	r.exits = [];
+
+    	var exits = $("input[name='exits[]']:checked");
+    	for( var e:any = 0; e < exits.length; e++) {
+    		r.exits.push(exits[e].value);
+    	}
+
+    	if(r.exits.length === 0) {
+    		this.alert('Rooms required exits');
+    	}
 
     	// Set Defaults
-    	t['hasMonster'] = t['hasMonster'] || false;
-    	t['hasTreasure'] = t['hasTreasure'] || false;
+    	r.hasMonster = t.hasMonster || false;
+    	r.hasTreasure = t.hasTreasure || false;
 
-    	this._rooms.push(t);
+    	this._rooms.push(r);
 
+    	console.log('rooms: ', this._rooms);
+    	this.listRooms();
+
+    	// var o = JSON.stringify({ "rooms" : this._rooms });
+    	// var b = new Blob([o],{type : 'text/json'});
+    	// saveAs(b, "rooms.json");
+	}
+
+	private saveRoomsFile() {
     	var o = JSON.stringify({ "rooms" : this._rooms });
     	var b = new Blob([o],{type : 'text/json'});
     	saveAs(b, "rooms.json");
+	}
+	private alert(msg) {
+		console.log(msg);
+		$( "#results" ).append( $('<span />').html(msg) );
+
 	}
 
 	private registerEvents() {
 		console.log('registering events');
 		$('#files').on('change', this._onFileChange);
-		$('#roomMaker #save').on('click',this._submitRoomConfig);
+		$('#roomMaker #create').on('click',this._createRoom);
+		$('#save').on('click',this._saveRooms);
 	}
 
 	public constructor() {
