c4c512e40d7da3171dbc53d45b23e9216ff874fc
Anthony Deaver <lordbishop@gmail.com>
2014-04-25 07:53:23 -0400

    unit test stuff

--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,43 @@
-/// <reference path="lib/qunit.d.ts" />
+/// <reference path="libs/qunit.d.ts" />
 
-QUnit.test("deepEqual test", function () {
-    var obj = { foo: "bar" };
+// QUnit.test('room', function() {
+// 	window.$event = new Events(); 
+// 	var rm = new Rooms('haunted_mansion');
+// 	var res = rm.getStartingRoom();
+// 	ok( 1 == "1", "Passed!" );
+// });
 
-    QUnit.deepEqual(obj, { foo: "bar" }, "Two objects can be the same in value");
+QUnit.module('Delve Events', {
+	setup: function() {
+		var result = this.result = [];
+		this.createHandler = function(id) {
+        return function() {
+          result.push([id].concat([].slice.call(arguments, 1)));
+        };
+      };
+	}
 });
+
+QUnit.test('handler order', function() {
+	window.$event = new Events(); 
+	$event.bind('order', this.createHandler('order1'));
+	$event.bind('order', this.createHandler('order2'));
+	$event.emit('order');
+	deepEqual(this.result,[
+		['order1'],
+		['order2']
+		], "event handlers are fired in the other they were created");
+});
+
+QUnit.test('arguments', function() {
+	window.$event = new Events(); 
+    $event.bind('arguments', this.createHandler('arguments1'));
+    $event.bind('arguments', this.createHandler('arguments2'));
+    $event.bind('arguments', this.createHandler('arguments3'));
+    $event.emit('arguments', 'gone');
+    deepEqual(this.result, [
+      ['arguments1', 'gone'],
+      ['arguments2', 'gone'],
+      ['arguments3', 'gone']
+    ], 'handlers should receive all passed arguments.');
+  });
